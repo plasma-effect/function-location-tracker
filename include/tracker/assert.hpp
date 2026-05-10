548636bf142c969a3cbb2b7b575b97ceef31eb1d
plasma-ep <mk_vertex3625_plasma@kcn.jp>
2026-05-10 18:16:46 +0900

    add assert

---
+++ include/tracker/assert.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "tracker/location.hpp"
+
+namespace location_tracker::detail {
+#ifdef LOCAL_DEBUG
+[[noreturn]] void
+fail(const char* expr, std::source_location loc,
+     std::vector<std::pair<std::string, std::string>> const& values);
+#define TRACKER_ASSERT(...)                                                    \
+  (static_cast<bool>(__VA_ARGS__)                                              \
+       ? void(0)                                                               \
+       : location_tracker::detail::fail(#__VA_ARGS__,                          \
+                                        std::source_location::current(), {}))
+#define TRACKER_EXPECT_1(value0, ...)                                          \
+  (static_cast<bool>(__VA_ARGS__)                                              \
+       ? void(0)                                                               \
+       : location_tracker::detail::fail(                                       \
+             #__VA_ARGS__, std::source_location::current(),                    \
+             {                                                                 \
+                 {#value0, std::format("{}", value0)},                         \
+  }))
+#define TRACKER_EXPECT_2(value0, value1, ...)                                  \
+  (static_cast<bool>(__VA_ARGS__)                                              \
+       ? void(0)                                                               \
+       : location_tracker::detail::fail(                                       \
+             #__VA_ARGS__, std::source_location::current(),                    \
+             {                                                                 \
+                 {#value0, std::format("{}", value0)},                         \
+                 {#value1, std::format("{}", value1)},                         \
+  }))
+#else
+#define TRACKER_ASSERT(...)                                                    \
+  assert(__VA_ARGS__);                                                         \
+  [[assume((__VA_ARGS__))]]
+#define TRACKER_EXPECT_1(value0, ...) TRACKER_ASSERT(__VA_ARGS__)
+#define TRACKER_EXPECT_2(value0, value1, ...) TRACKER_ASSERT(__VA_ARGS__)
+#endif
+} // namespace location_tracker::detail
---
+++ src/assert.cpp
@@ -0,0 +1,19 @@
+#include "tracker/assert.hpp"
+
+namespace location_tracker::detail {
+[[noreturn]] void
+fail(const char* expr, std::source_location loc,
+     std::vector<std::pair<std::string, std::string>> const& values) {
+  std::println(std::cerr, "assertion failed: \"{}\"", expr);
+  for (auto const& [name, expr] : values) {
+    std::println(" {} = {}", name, expr);
+  }
+  std::println(std::cerr, "where: line {} in {}", loc.line(),
+               loc.function_name());
+  for (auto floc : get_locations() | std::views::reverse) {
+    std::println(std::cerr, " from: line {} in {}", floc.line(),
+                 floc.function_name());
+  }
+  throw std::logic_error("location_tracker assertion failed");
+}
+} // namespace location_tracker::detail
